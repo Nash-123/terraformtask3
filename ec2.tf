1640c351102a591a9391f12b64a14b887cdb2140
Nash-123 <58680481+Nash-123@users.noreply.github.com>
2020-07-26 18:24:22 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,51 @@
+variable "own_key_name"{}
+
+//  creation of the public subnet instance
+
+variable "own_public_instance_ami"{}
+variable "own_public_instance_type"{}
+resource "aws_instance" "public_web" {
+  depends_on=[aws_security_group.allow_public_web]
+  ami           = var.own_public_instance_ami      
+  instance_type = var.own_public_instance_type
+  subnet_id= aws_subnet.public_subnet.id 
+  vpc_security_group_ids=[ aws_security_group.allow_public_web.id ]
+  
+  key_name=var.own_key_name
+  tags = {
+    Name = "public_web"
+  }
+}
+
+//creation of the bostion_host
+
+variable "own_bositon_instance_ami"{}
+variable "own_bositon_instance_type"{}
+resource "aws_instance" "bositon_host" {
+depends_on=[aws_security_group.only_ssh_bositon]
+  ami           = var.own_bositon_instance_ami
+  instance_type = var.own_bositon_instance_type
+  subnet_id= aws_subnet.public_subnet.id 
+  vpc_security_group_ids=[ aws_security_group.only_ssh_bositon.id]
+  key_name=var.own_key_name
+  tags = {
+    Name = "bositon_host"
+  }
+}
+
+
+//  creating privatesubnet instance 
+
+variable "own_private_instance_type"{}
+variable "own_private_instance_ami"{}
+resource "aws_instance" "my_sql" {
+depends_on=[aws_security_group.only_sql_web,aws_security_group.only_ssh_sql_bositon]
+  ami           = var.own_private_instance_ami   
+  instance_type = var.own_private_instance_type
+  subnet_id= aws_subnet.private_subnet.id 
+  vpc_security_group_ids=[ aws_security_group.only_sql_web.id ,aws_security_group.only_ssh_sql_bositon.id]
+  key_name=var.own_key_name
+  tags = {
+    Name = "my_sql"
+  }
+}
